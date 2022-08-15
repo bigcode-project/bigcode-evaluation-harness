86967328337b3c63e630fcec56abdfcb3e013158
loubnabnl <loubnabenallal1999@gmail.com>
2022-08-15 00:11:08 +0000

    add two mbpp prompt generation functions, add proper post-processing, prompt prefix and same generation function

--- lm_eval/utils.py
+++ lm_eval/utils.py
@@ -9,9 +9,10 @@ from torch.utils.data import IterableDataset
 from datasets import load_dataset
 
 EOF_STRINGS = ["\nclass", "\ndef", "\n#", "\n@", "\nprint", "\nif"]
+MBPP_EOF_STRINGS = ["\nclass", "\nassert", '\n"""', "\nprint", "\nif"]
 
 
-def truncate_prompt(prompt, tokenizer, max_length, call_format):
+def truncate_prompt_apps(prompt, tokenizer, max_length, call_format):
     # if a prompt is very long we truncate it but keep the end phrases
     input_ids = tokenizer(prompt, return_tensors="pt").input_ids[0]
     if len(input_ids) > max_length:
@@ -22,7 +23,19 @@ def truncate_prompt(prompt, tokenizer, max_length, call_format):
     return prompt
 
 
-def generate_prompt_apps(sample, tokenizer, max_length=1024):
+def first_block(string, stop_words):
+    """Split off first block of code by scanning for class, def etc. on newlines."""
+    return re.split("|".join(stop_words), string)[0].rstrip()
+
+
+def remove_last_block(string, stop_words):
+    """Remove the last block of the code containing stop_words for HumanEval and MBPP"""
+    string_list = re.split("(%s)" % "|".join(stop_words), string)
+    # last string should be ""
+    return "".join(string_list[:-2])
+
+
+def generate_prompt_apps(sample, tokenizer, max_length=1024, prefix=""):
     """Generate prompts for APPS, they include a question along with some starter code and function name if they exist
     We also specify the type of the prompt, i.e. whether it is call-based or standard input"""
 
@@ -43,27 +56,34 @@ def generate_prompt_apps(sample, tokenizer, max_length=1024):
         call_format = "\nUse Call-Based format"
         prompt += call_format
     prompt += "\nANSWER:\n"
-    prompt = truncate_prompt(prompt, tokenizer, max_length, call_format)
-    return prompt
+    prompt = truncate_prompt_apps(prompt, tokenizer, max_length, call_format)
+    return prefix + prompt
 
 
-def generate_prompt_mbpp(sample):
-    """Generate prompts for MBPP prompt similarily to the original paper:
+def mbpp_incoder_prompt(sample, include_solution_mbpp=False, prefix=""):
+    """Generate prompts for MBPP prompt similarily to InCoder, docstring
+    that includes one test"""
+    description = sample['text']
+    test_example = sample['test_list'][0]
+    prompt = f'"""\n{description}\n{test_example}\n"""\n'
+
+    if include_solution_mbpp:
+        prompt += f"{sample['code']}\n"
+    return prefix + prompt
+
+
+def mbpp_google_prompt(sample, include_tests=True, prefix=""):
+    """Generate prompts for MBPP prompt similarily to the original google paper
+    with an option for including the tests cases or not:
     prompt = description + 'Your code should
     satisfy these tests:'+ three assert statements"""
 
-    prompt = sample["text"] + " Your code should satisfy these tests:\n"
-    for test in sample["test_list"]:
-        prompt += "\n" + test
-    return prompt
-
-
-def remove_last_block(string):
-    """Remove the last block of the code containing EOF_STRINGS for HumanEval"""
-    
-    string_list = re.split("(%s)" % "|".join(EOF_STRINGS), string)
-    # last string should be ""
-    return "".join(string_list[:-2])
+    prompt = sample["text"]
+    if include_tests:
+        prompt += " Your code should satisfy these tests:\n"
+        for test in sample["test_list"]:
+            prompt += "\n" + test
+    return prefix + prompt
 
 
 class TokenizedDataset(IterableDataset):
@@ -72,26 +92,43 @@ class TokenizedDataset(IterableDataset):
     See compute_code for more details.
     """
 
-    def __init__(self, tokenizer, dataset, mode="humaneval", n_tasks=None, n_copies=1, max_length=1024):
+    def __init__(self,
+                tokenizer,
+                dataset,
+                mode="humaneval",
+                n_tasks=None,
+                n_copies=1,
+                max_length_prompt=1024,
+                include_tests_mbpp=True,
+                include_solution_mbpp=False,
+                prompt_type_mbpp="incoder",
+                prefix=""):
+
         self.tokenizer = tokenizer
         self.dataset = dataset
         self.mode = mode
         self.n_tasks = len(dataset) if n_tasks is None else n_tasks
         self.n_copies = n_copies
-        self.max_length = 1024
-        
+        self.max_length_prompt = max_length_prompt
+        self.include_tests_mbpp = include_tests_mbpp
+        self.include_solution_mbpp = include_solution_mbpp
+        self.prompt_type_mbpp = prompt_type_mbpp
+        self.prefix = prefix
+
     def __iter__(self):
         prompts = []
         for task in range(self.n_tasks):
             if self.mode == "apps":
-                prompt = generate_prompt_apps(self.dataset[task], self.tokenizer, self.max_length)
+                prompt = generate_prompt_apps(self.dataset[task], self.tokenizer, self.max_length_prompt)
             elif self.mode == "mbpp":
-                prompt = generate_prompt_mbpp(self.dataset[task])
+                if self.prompt_type_mbpp == "incoder":
+                    prompt = mbpp_incoder_prompt(self.dataset[task], self.include_solution_mbpp, prefix=self.prefix)
+                else:
+                    prompt = mbpp_google_prompt(self.dataset[task], self.include_tests_mbpp, prefix = self.prefix)
             else:
                 prompt = self.dataset[task]["prompt"].strip()
-            prompt = self.tokenizer.eos_token + prompt
             prompts.append(prompt)
-        outputs = self.tokenizer(prompts, padding=True, truncation=True, return_tensors="pt", max_length=self.max_length)
+        outputs = self.tokenizer(prompts, padding=True, truncation=True, return_tensors="pt", max_length=self.max_length_prompt)
         for task in range(self.n_tasks):
             for _ in range(self.n_copies):
                 yield {
@@ -101,15 +138,28 @@ class TokenizedDataset(IterableDataset):
                 }
 
 
-def complete_code(accelerator, model, tokenizer, dataloader, n_tasks, batch_size=20, mode="humaneval", **gen_kwargs):
+def complete_code(accelerator,
+                  model,
+                  tokenizer,
+                  dataloader,
+                  n_tasks,
+                  batch_size=20,
+                  mode="humaneval",
+                  include_tests_mbpp=True,
+                  include_solution_mbpp=False,
+                  prompt_type_mbpp="incoder",
+                  prefix="",
+                  **gen_kwargs):
+
     """Generate multiple codes for each task in the dataset using multiple GPUs with accelerate.
     dataloader sends all the prompts from the evalution dataset to the model as the following:
     [p_0_0, p_0_1, ..., p_0_nc-1, p_1_0, ..., p_nt-1_nc-1] where nc is the number of copies of the prompt,
     and nt is the number of tasks. nc is such that num_samples(for each task)= nc * batch_size
     """
+
     if mode == "mbpp":
         MBPP = load_dataset("mbpp", split="test", ignore_verifications=True)
-        # the MBPP evaluation set is task ids 11->510 
+        # the MBPP evaluation set is task ids 11->510
         MBPP = MBPP.select([i for i in range(10,510)])
 
     gen_token_dict = defaultdict(list)  # dict of list of generated tokens
@@ -138,14 +188,18 @@ def complete_code(accelerator, model, tokenizer, dataloader, n_tasks, batch_size
         for s in generated_tokens:
             gen_code = tokenizer.decode(s, skip_special_tokens=True, clean_up_tokenization_spaces=True)
             if mode == "humaneval":
-                code_gens[task].append(remove_last_block(gen_code))
+                code_gens[task].append(remove_last_block(gen_code, EOF_STRINGS))
             elif mode == "apps":
                 try:
-                    code_gens[task].append(gen_code.split("ANSWER:")[1].replace(tokenizer.eos_token, ""))
+                    code_gens[task].append(gen_code.split("\nANSWER:\n", 1)[1])
                 except IndexError:
-                    print(f"Index error for task {task}!!!!")
+                    print(f"Index error for task {task}!")
                     code_gens[task].append(gen_code.replace(tokenizer.eos_token, ""))
             else:
-                prompt = generate_prompt_mbpp(MBPP[task])
-                code_gens[task].append(gen_code[len(prompt):].replace(tokenizer.eos_token, ""))
+                if prompt_type_mbpp == "incoder":
+                    prompt = mbpp_incoder_prompt(MBPP[int(task)], include_solution_mbpp, prefix)
+                else:
+                    prompt = mbpp_google_prompt(MBPP[int(task)], include_tests_mbpp, prefix)
+                output = gen_code[len(prompt):]
+                code_gens[task].append(first_block(output, MBPP_EOF_STRINGS))
     return code_gens
