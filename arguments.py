63f93bdecc4f3a48b68d18a97816b4dcc412a008
loubnabnl <loubnabenallal1999@gmail.com>
2022-08-15 00:08:52 +0000

    add more arguments

--- arguments.py
+++ arguments.py
@@ -12,7 +12,7 @@ class EvalArguments:
         default="codeparrot/codeparrot", metadata={"help": "Model name in Hugging Face hub or its local path."}
     )
     num_workers: Optional[int] = field(default=None, metadata={"help": "Number of workers used for code evaluation."})
-    num_tasks_humaneval: Optional[int] = field(
+    num_tasks_he: Optional[int] = field(
         default=None,
         metadata={"help": "The number of human-eval tasks to run. If not specified all tasks are evaluated."},
     )
@@ -24,16 +24,34 @@ class EvalArguments:
         default=None,
         metadata={"help": "The number of APPS tasks to run. If not specified all tasks are evaluated."},
     )
+    include_tests_mbpp: Optional[bool] = field(
+        default=True,
+        metadata={"help": "Whether to include test cases in the prompt for MBPP."},
+    )
+    include_solution_mbpp: Optional[bool] = field(
+        default=False,
+        metadata={"help": "Whether to include a solution in the prompt for MBPP in InCoder style."},
+    )
+    prompt_type_mbpp: Optional[str] = field(
+        default="incoder",
+        metadata={"help": "The type of prompt to use for MBPP. Can be 'incoder' or 'google'."},
+    )
+    prefix: Optional[str] = field(default="", metadata={"help": "Prefix to add to the generated code. For exmaple InCoder need prefix='<| file ext=.py |>\n'"})
     level_apps: Optional[str] = field(
         default="all",
         metadata={"help": "The difficulty level to use for APPS, among introductory, interview, competition and all."},
     )
+    max_length_prompt: Optional[int] = field(
+        default=1024,
+        metadata={"help": "The maximum length of the prompt."},
+    )
+    max_new_tokens_he: Optional[int] = field(default=256, metadata={"help": "Maximum number of newly generated tokens, used in humaneval."})
+    max_new_tokens_apps: Optional[int] = field(default=900, metadata={"help": "Maximum number of newly generated tokens, used in apps."})
+    max_new_tokens_mbpp: Optional[int] = field(default=450, metadata={"help": "Maximum number of newly generated tokens, used in mbpp."})
     do_sample: Optional[bool] = field(
         default=True, metadata={"help": "Sample from the language model's output distribution."}
     )
     temperature: Optional[float] = field(default=0.2, metadata={"help": "Sampling temperature used for generation."})
-    max_new_tokens: Optional[int] = field(default=256, metadata={"help": "Maximum number of newly generated tokens, used in humaneval."})
-    max_length: Optional[int] = field(default=1024, metadata={"help": "Maximum length of prompt+generated sequence, used in APPS and MBPP"})
     top_k: Optional[int] = field(default=0, metadata={"help": "Top-k parameter used for generation."})
     top_p: Optional[float] = field(default=0.95, metadata={"help": "Top-p parameter used for nucleus sampling."})
     n_samples: Optional[int] = field(
