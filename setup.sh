576e8df9232aa61b67ba582d591474f316e1af3c
Loubna Ben Allal <44069155+loubnabnl@users.noreply.github.com>
2022-10-21 15:21:14 +0200

    change generations

--- setup.sh
+++ setup.sh
@@ -11,11 +11,11 @@ ulimit -n 50000
 
 echo downloading MBPP generations from the hub and copying them to current directory
 git clone https://huggingface.co/datasets/loubnabnl/code-generations-bigcode
-cp code-generations-bigcode/anylicense-02/generations.json bigcode-evaluation-harness/
+cp code-generations-bigcode/codeparrotdedup-08/generations.json bigcode-evaluation-harness/
 
 echo small test run on 4 tasks
 cd bigcode-evaluation-harness
-accelerate launch  main.py   --tasks mbpp   --prompt_type_mbpp "incoder" --allow_code_execution=True --evaluation_only True   --model gpt2-any-license-02  --num_tasks_mbpp 4
+accelerate launch  main.py   --tasks mbpp   --prompt_type_mbpp "incoder" --allow_code_execution=True --evaluation_only True   --model codeparrotdedup-08  --num_tasks_mbpp 4
 
 echo running evaluation on all 500 tasks
-accelerate launch  main.py   --tasks mbpp   --prompt_type_mbpp "incoder" --allow_code_execution=True --evaluation_only True   --model gpt2-any-license-02
+accelerate launch  main.py   --tasks mbpp   --prompt_type_mbpp "incoder" --allow_code_execution=True --evaluation_only True   --model codeparrotdedup-08
