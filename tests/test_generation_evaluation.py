ea26d2153f00fed818c72280668cacb75cd0bfe6
loubnabnl <loubnabenallal1999@gmail.com>
2022-10-24 15:28:35 +0200

    update tests

--- tests/test_generation_evaluation.py
+++ tests/test_generation_evaluation.py
@@ -4,9 +4,9 @@ import tempfile
 
 from accelerate import Accelerator
 from accelerate.utils import write_basic_config
-from transformers import AutoModelForCausalLM, AutoTokenizer
+from transformers import AutoModelForCausalLM, AutoTokenizer, set_seed
 
-from arguments import EvalArguments
+from lm_eval.arguments import EvalArguments
 from lm_eval.evaluator import Evaluator
 
 # TODO add folder for example generations + scores for evaluation mode
@@ -16,13 +16,19 @@ TMPDIR = tempfile.mkdtemp()
 TEST_MODEL = "hf-internal-testing/tiny-random-gpt2"
 
 def update_args(args):
-    args.model = "codeparrot/codeparrot-small"
+    args.model = "hf-internal-testing/tiny-random-gpt2"
+    # the executed code for the tests is safe (see tests/data/*_eval_gens.json)
     args.allow_code_execution = True
     args.save_generations = False
+    args.save_references = False
     args.output_path = TMPDIR
+    args.evaluation_only = False
+    args.generation_only = False
+    # postprocessing for HumanEval and MBPP makes generations with dummy model not distinctive
+    args.postprocess = False
 
     args.batch_size = 1
-    args.max_length_generation = 128
+    args.max_length_generation = 300
     args.do_sample = False
     args.top_p = 0
     args.n_samples = 1
@@ -35,12 +41,16 @@ def update_args(args):
     args.num_tasks_concode = 2
     args.num_tasks_conala = 2
     args.num_tasks_spider = 2
+
+    args.language = "python"
+    args.setup_apps = "finetuning"
     return args
 
 
 def setup():
     model = AutoModelForCausalLM.from_pretrained(TEST_MODEL)
     tokenizer = AutoTokenizer.from_pretrained(TEST_MODEL)
+    tokenizer.pad_token = tokenizer.eos_token
     configPath = os.path.join(TMPDIR, "default_config.yml")
     write_basic_config(save_location=configPath)
     accelerator = Accelerator()
@@ -49,21 +59,24 @@ def setup():
 
 def load_generation_examples(task):
     # generations for testing the generation feature of dummy test model
-    with open(f"tests/data/{task}_gen_refs.json") as fp:
-        generations = json.load(fp)
     with open(f"tests/data/{task}_gen_gens.json") as fp:
+        generations = json.load(fp)
+    with open(f"tests/data/{task}_gen_refs.json") as fp:
         references = json.load(fp)
     return generations, references
 
 # TEST SUITE
 args = update_args(EvalArguments())
+set_seed(args.seed)
 model, tokenizer, accelerator = setup()
+
 
 
 def test_generation():
     args.generation_only = True
     evaluator = Evaluator(accelerator, model, tokenizer, args)
     for task in TASKS:
+        print(f"testing task {task}")
         generations, references = evaluator.generate_text(task)
         true_gens, true_refs = load_generation_examples(task)
         assert generations == true_gens
@@ -73,17 +86,14 @@ def test_generation():
 def test_evaluation():
     # TODO add scores for each task
     args.evaluation_only = True
+    args.n_sample = 2
+    TASKS = ["humaneval"]
     for task in TASKS:
         # path to generation examples to evaluate (for which we know the scores)
-        args.generations_path(f"tests/data/{task}_eval_gens.json")
+        args.generations_path = f"tests/data/{task}_eval_gens.json"
         evaluator = Evaluator(accelerator, None, None, args)
         results = evaluator.evaluate(task)
-        if task == "mbpp":
-            assert results["mbpp"]["mbpp"] == {
-                                "pass@1": 0.04344000000000002,
-                                "pass@10": 0.21504027174321383,
-                                "pass@100": 0.4543711495045223
-                                }
+        if task == "humaneval":
+            assert results == {'pass@1': 0.25}
         else:
             pass
-
