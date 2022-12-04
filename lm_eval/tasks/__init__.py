76e1040173d4cb5636a217e575524334c4b45240
loubnabnl <loubnabenallal1999@gmail.com>
2022-12-04 17:47:53 +0100

    update init file

--- lm_eval/tasks/__init__.py
+++ lm_eval/tasks/__init__.py
@@ -3,11 +3,19 @@ from pprint import pprint
 from . import humaneval
 from . import mbpp
 from . import apps
+from . import concode
+from . import conala
+from . import code_to_text
+from . import code_to_text_python_left
 
 TASK_REGISTRY = {
     "humaneval": humaneval.HumanEval,
     "mbpp": mbpp.MBPP,
     **apps.create_all_tasks(),
+    "concode": concode.Concode,
+    "conala": conala.Conala,
+    **code_to_text.create_all_tasks(),
+    "code_to_text_python_left": code_to_text_python_left.CodeToTextLeft,
 }
 
 ALL_TASKS = sorted(list(TASK_REGISTRY))
