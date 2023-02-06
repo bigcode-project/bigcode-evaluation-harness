3e12b390986bba7b6af5e1c3dd1363d7cda338c7
Logesh Kumar Umapathi <infinitylogesh@gmail.com>
2023-02-06 11:41:11 +0000

    gsm8k and gsmhard tasks added

--- lm_eval/tasks/__init__.py
+++ lm_eval/tasks/__init__.py
@@ -1,6 +1,6 @@
 from pprint import pprint
 
-from . import apps, codexglue_code_to_text, conala, concode, humaneval, mbpp, codexglue_text_to_text
+from . import apps, codexglue_code_to_text, conala, concode, gsm, humaneval, mbpp, codexglue_text_to_text
 
 TASK_REGISTRY = {
     **apps.create_all_tasks(),
@@ -11,6 +11,7 @@ TASK_REGISTRY = {
     "concode": concode.Concode,
     "humaneval": humaneval.HumanEval,
     "mbpp": mbpp.MBPP,
+    **gsm.create_all_tasks(),
 }
 
 ALL_TASKS = sorted(list(TASK_REGISTRY))
