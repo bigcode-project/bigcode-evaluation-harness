6141ab28c180f7d370152d1f320a0b071914c2e2
Niklas Muennighoff <muennighoff@Niklass-Air.home>
2023-03-24 20:31:26 +0100

    Write out exception

--- lm_eval/base.py
+++ lm_eval/base.py
@@ -25,9 +25,9 @@ class Task(ABC):
         self.requires_execution = requires_execution
         try:
             self.dataset = load_dataset(path=self.DATASET_PATH, name=self.DATASET_NAME)
-        except:
+        except Exception as e:
             warn(
-                "This task will use a locally downloaded dataset, not from the HF hub."
+                f"Loading the dataset failed with {str(e)}. This task will use a locally downloaded dataset, not from the HF hub."
             )
 
     @abstractmethod
