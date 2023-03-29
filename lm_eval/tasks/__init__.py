83698f392528196cb7a2543db0b55cd07a9d3c60
Muennighoff <n.muennighoff@gmail.com>
2023-03-29 21:42:50 +0200

    Add HeXBugs

--- lm_eval/tasks/__init__.py
+++ lm_eval/tasks/__init__.py
@@ -2,7 +2,7 @@ import inspect
 from pprint import pprint
 
 from . import (apps, codexglue_code_to_text, codexglue_text_to_text, conala,
-               concode, ds1000, humaneval, mbpp, parity, python_bugs, quixbugs)
+               concode, ds1000, humaneval, humaneval_x_bugs, mbpp, parity, python_bugs, quixbugs)
 
 TASK_REGISTRY = {
     **apps.create_all_tasks(),
@@ -13,6 +13,7 @@ TASK_REGISTRY = {
     "concode": concode.Concode,
     **ds1000.create_all_tasks(),
     "humaneval": humaneval.HumanEval,
+    "humaneval_x_bugs": humaneval_x_bugs.HumanEvalXBugs,
     "mbpp": mbpp.MBPP,
     "parity": parity.Parity,
     "python_bugs": python_bugs.PythonBugs,
