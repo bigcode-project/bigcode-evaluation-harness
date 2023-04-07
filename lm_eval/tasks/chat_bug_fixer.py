7316a86884b4fbdae8e81cc49b21cc9a9d1152ba
mitya <dimitry.ageev@gmail.com>
2023-04-07 23:26:15 +0300

    separate parsing and querying, save raw chat response to improve parsing

--- lm_eval/tasks/chat_bug_fixer.py
+++ lm_eval/tasks/chat_bug_fixer.py
@@ -4,6 +4,7 @@ import jsonlines
 import termcolor
 
 from cdifflib import CSequenceMatcher
+from camel_converter import to_snake
 from datasets import load_dataset
 from typing import List, Dict
 
@@ -32,34 +33,43 @@ class ParseError(Exception):
     pass
 
 
-class ChatWrapper:
-
-    def __init__(self,
-                 model: str,
-                 messages: List[Dict[str, str]],
-                 stop_on_parse_error: bool = False):
-        self._model = model
-        self._messages = messages
-        self._stop_on_parse_error = stop_on_parse_error
-        self._parse_errors = 0
-        self._query_counter = 0
+class ContentParser:
 
     @staticmethod
-    def _parse_content(prompt: str, content: str):
+    def _entry_point_variations(entry_point: str) -> List[str]:
+        # NOTE: workaround dataset's bug with entry point naming
+        return [
+            entry_point,
+            to_snake(entry_point),
+            entry_point[0].lower() + entry_point[1:],
+        ]
+
+    def __call__(self, prompt: str, content: str, entry_point: str):
         # NOTE: Model doesn't follow instructions directly:
         # adds description of change and sometimes fixes
         # typos, or other "bugs" in description.
         if "```" in content:
             content = content.split("```")[1]
+        # first parse with assumption that content has description
         matcher = CSequenceMatcher(None, prompt, content)
         tag, _, _, j1, j2 = matcher.get_opcodes()[-1]
-        if tag != "insert":
-            raise ParseError(f"prompt not found in content:\n{content}")
-        return content[j1:j2]
+        if tag == "insert":
+            return content[j1:j2]
+        # second parse content with assumption that model wrote code without description
+        for entry_point in self._entry_point_variations(entry_point):
+            if entry_point in content:
+                content = content.split(entry_point)[1]
+                return "".join(content.splitlines(keepends=True)[1:])
+        raise ParseError(f"prompt is not in content:\n{content}")
 
-    @property
-    def parse_error_rate(self) -> float:
-        return self._parse_errors / self._query_counter
+
+class ChatWrapper:
+
+    def __init__(self,
+                 model: str,
+                 messages: List[Dict[str, str]]):
+        self._model = model
+        self._messages = messages
 
     def __call__(self, prompt: str, solution: str) -> str:
         messages = [
@@ -71,18 +81,12 @@ class ChatWrapper:
         ]
         while True:
             try:
-                self._query_counter += 1
                 response = openai.ChatCompletion.create(
                     model=self._model,
                     messages=messages)
                 message = response["choices"][0]["message"]
                 assert message["role"] == "assistant"
-                return self._parse_content(prompt, message["content"])
-            except ParseError as e:
-                self._parse_errors += 1
-                print("PARSE EXCEPTION:", e)
-                if self._stop_on_parse_error:
-                    return ""
+                return message["content"]
             except Exception as e:
                 print("API EXCEPTION:", e)
 
@@ -90,32 +94,34 @@ class ChatWrapper:
 if __name__ == '__main__':
     TIMES = 1
     VERBOSE = True
+    LANGUAGE = "python"
 
     openai.organization = os.getenv("OPENAI_ORGANIZATION")
     openai.api_key = os.getenv("OPENAI_API_KEY")
-    samples = [s for s in load_dataset("bigcode/humaneval-x-bugs", "python")["test"]] * TIMES
+
+    samples = [s for s in load_dataset("bigcode/humaneval-x-bugs", LANGUAGE)["test"]] * TIMES
 
     chat_wrapper = ChatWrapper("gpt-3.5-turbo", messages)
+    parse_errors = 0
+    parser = ContentParser()
     for idx, sample in enumerate(samples):
         if VERBOSE:
             print(f"Processing {sample['task_id']} ({idx + 1}/{len(samples)}))...")
+            print(termcolor.colored(sample["entry_point"], "yellow", attrs=["bold"]))
             print(termcolor.colored(sample["prompt"], "yellow"))
             print(termcolor.colored(sample["buggy_solution"], "red"))
-        sample["completion"] = chat_wrapper(sample["prompt"], sample["buggy_solution"])
+        sample["raw_generation"] = chat_wrapper(sample["prompt"], sample["buggy_solution"])
+        try:
+            sample["generation"] = parser(sample["prompt"], sample["raw_generation"], sample["entry_point"])
+        except ParseError as e:
+            parse_errors += 1
+            print("PARSE EXCEPTION:", e)
+            sample["generation"] = ""
         if VERBOSE:
-            print(termcolor.colored(sample["completion"], "green"))
+            print(termcolor.colored(sample["generation"], "green"))
+    if VERBOSE:
+        print("parse error rate:", parse_errors / len(samples))
 
-    results_filename = "completions.jsonl"
+    results_filename = f"completions_{LANGUAGE}.jsonl"
     with jsonlines.open(results_filename, "w") as writer:
-        # NOTE: compatibility with humaneval format
-        for sample in samples:
-            sample["task_id"] = sample["task_id"].replace("Python", "HumanEval")
         writer.write_all(samples)
-
-    # NOTE: sample code to run evaluation
-    import subprocess
-    res = subprocess.check_output(
-        f"evaluate_functional_correctness {results_filename}",
-        shell=True, stderr=subprocess.DEVNULL)
-    print("parse error rate:", chat_wrapper.parse_error_rate)
-    print(res.decode("utf8"))
