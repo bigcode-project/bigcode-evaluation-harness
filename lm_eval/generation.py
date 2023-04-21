2d9d9e58715acf42daef50c002db13be9ebb1604
loubnabnl <loubnabenallal1999@gmail.com>
2023-04-21 09:07:38 +0000

    use accelerate device

--- lm_eval/generation.py
+++ lm_eval/generation.py
@@ -78,14 +78,16 @@ def parallel_generations(task, dataset, accelerator, model, tokenizer, n_tasks, 
 
     # do not confuse args.batch_size, which is actually the num_return_sequences
     ds_loader = DataLoader(ds_tokenized, batch_size=1)
+
+    device = accelerator.device
     if args.precision == "bf16":
         print("Converting model to bf16")
-        model = model.bfloat16().cuda()
+        model = model.bfloat16().to(device)
     elif args.precision == "fp16":
         print("Converting model to fp16")
-        model = model.half().cuda()
+        model = model.half().to(device)
     else:
-        model = model.cuda()
+        model = model.to(device)
 
     ds_loader = accelerator.prepare(ds_loader)
     generations = complete_code(
