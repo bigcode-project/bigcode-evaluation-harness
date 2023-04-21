9396f6db1eeafcf1e4b172b5193f7482d97e96df
Loubna Ben Allal <44069155+loubnabnl@users.noreply.github.com>
2023-04-21 14:53:57 +0200

    remove prompt from generation

--- lm_eval/tasks/humaneval.py
+++ lm_eval/tasks/humaneval.py
@@ -77,6 +77,7 @@ class HumanEval(Task):
             (not used for Humaneval-Task)
         """
         prompt = self.get_prompt(self.dataset["test"][idx])
+        generation = generation[len(prompt) :]
         return prompt + self._stop_at_stop_token(generation, self.stop_words)
 
     def process_results(self, generations, references):
