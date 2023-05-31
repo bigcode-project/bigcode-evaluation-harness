a6ac2114a011346b3b1cff127862de2c694e2e62
sedrickkeh <sedrickkeh@gmail.com>
2023-05-30 18:35:35 -0700

    enforce tokenizer padding_side="right"

--- main.py
+++ main.py
@@ -186,6 +186,7 @@ def main():
             trust_remote_code=args.trust_remote_code,
             use_auth_token=args.use_auth_token,
             truncation_side="left",
+            padding_side="right",
         )
         if not tokenizer.eos_token:
             if tokenizer.bos_token:
