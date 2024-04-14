4b1c8fca4028b7141599cb88be0c6d56722a2707
Mingzhe Du <dumingzhe@126.com>
2024-04-14 09:40:40 +0000

    init commit

---
+++ bigcode_eval/tasks/custom_metrics/beyond_eval.py
@@ -0,0 +1,406 @@
+# coding: utf-8
+
+# The Beyond metric estimates the beyond@k metric for code synthesis efficiency.
+
+CITATION = """
+@article{du2024mercury,
+    title={Mercury: An Efficiency Benchmark for LLM Code Synthesis},
+    author={Du, Mingzhe and Luu, Anh Tuan and Ji, Bin and Ng, See-Kiong},
+    journal={arXiv preprint arXiv:2402.07844},
+    year={2024}
+}
+"""
+
+import io
+import os
+import time
+import math
+import json
+import signal
+import tempfile
+import platform
+import itertools
+import contextlib
+import faulthandler
+import numpy as np
+from tqdm import tqdm
+from multiprocessing import Manager, Process
+from typing import Optional, Dict, Any, List
+from concurrent.futures import ThreadPoolExecutor, as_completed
+
+
+# Timeout Exception
+class TimeoutException(Exception):
+    """ Raise for TimeoutException """
+    pass
+
+# Redirect stdin
+class RedirectStdin(contextlib._RedirectStream):
+    """Context manager for temporarily receiving stdin from another source."""
+    _stream = 'stdin'
+    
+# WriteOnly IO
+class WriteOnlyStringIO(io.StringIO):
+    """ StringIO that throws an exception when it's read from """
+
+    def read(self, *args, **kwargs):
+        raise IOError
+
+    def readline(self, *args, **kwargs):
+        raise IOError
+
+    def readlines(self, *args, **kwargs):
+        raise IOError
+
+    def readable(self, *args, **kwargs):
+        """ Returns True if the IO object can be read. """
+        return False
+    
+class Sandbox(object):
+    @staticmethod
+    @contextlib.contextmanager
+    def time_limit(seconds: float):
+        def signal_handler(signum, frame):
+            raise TimeoutException("Timed out!")
+
+        signal.setitimer(signal.ITIMER_REAL, seconds)
+        signal.signal(signal.SIGALRM, signal_handler)
+        try:
+            yield
+        finally:
+            signal.setitimer(signal.ITIMER_REAL, 0)
+
+    @staticmethod
+    @contextlib.contextmanager
+    def swallow_io():
+        stream = WriteOnlyStringIO()
+        with contextlib.redirect_stdout(stream):
+            with contextlib.redirect_stderr(stream):
+                with RedirectStdin(stream):
+                    yield
+
+    @staticmethod
+    @contextlib.contextmanager
+    def create_tempdir():
+        with tempfile.TemporaryDirectory() as dirname:
+            with Sandbox.chdir(dirname):
+                yield dirname
+
+    @staticmethod
+    @contextlib.contextmanager
+    def chdir(root):
+        if root == ".":
+            yield
+            return
+        cwd = os.getcwd()
+        os.chdir(root)
+        try:
+            yield
+        except BaseException as exc:
+            raise exc
+        finally:
+            os.chdir(cwd)
+
+    @staticmethod
+    def reliability_guard(maximum_memory_bytes: Optional[int] = None):
+        """
+        This disables various destructive functions and prevents the generated code from interfering with the test (e.g. fork bomb, killing other processes, removing filesystem files, etc.)
+
+        WARNING
+        This function is NOT a security sandbox. Untrusted code, including, model-generated code, should not be blindly executed outside of one. See the Codex paper for more information about OpenAI's code sandbox, and proceed with caution.
+        """
+
+        if maximum_memory_bytes is not None:
+            import resource
+            resource.setrlimit(resource.RLIMIT_AS, (maximum_memory_bytes, maximum_memory_bytes))
+            resource.setrlimit(resource.RLIMIT_DATA, (maximum_memory_bytes, maximum_memory_bytes))
+            if platform.uname().system != 'Darwin':
+                resource.setrlimit(resource.RLIMIT_STACK, (maximum_memory_bytes, maximum_memory_bytes))
+
+        faulthandler.disable()
+
+        import builtins
+        builtins.exit = None
+        builtins.quit = None
+
+        import os
+        os.environ['OMP_NUM_THREADS'] = '1'
+
+        os.kill = None
+        os.system = None
+        os.putenv = None
+        os.remove = None
+        os.removedirs = None
+        os.rmdir = None
+        os.fchdir = None
+        os.setuid = None
+        os.fork = None
+        os.forkpty = None
+        os.killpg = None
+        os.rename = None
+        os.renames = None
+        os.truncate = None
+        os.replace = None
+        os.unlink = None
+        os.fchmod = None
+        os.fchown = None
+        os.chmod = None
+        os.chown = None
+        os.chroot = None
+        os.fchdir = None
+        os.lchflags = None
+        os.lchmod = None
+        os.lchown = None
+        os.getcwd = None
+        os.chdir = None
+
+        import shutil
+        shutil.rmtree = None
+        shutil.move = None
+        shutil.chown = None
+
+        import subprocess
+        subprocess.Popen = None  # type: ignore
+
+        __builtins__['help'] = None
+
+        import sys
+        sys.modules['ipdb'] = None
+        sys.modules['joblib'] = None
+        sys.modules['resource'] = None
+        sys.modules['psutil'] = None
+        sys.modules['tkinter'] = None
+             
+    @staticmethod
+    def unsafe_execute(sample, result):
+        with Sandbox.create_tempdir():
+
+            # These system calls are needed when cleaning up tempdir.
+            import os
+            import shutil
+            rmtree = shutil.rmtree
+            rmdir = os.rmdir
+            chdir = os.chdir
+
+            # Disable functionalities that can make destructive changes to the test.
+            Sandbox.reliability_guard()
+
+            try:
+                # Global Namespace
+                namespace = {}
+                exec("import re", namespace)
+                exec("import itertools", namespace)
+                exec("import collections", namespace)
+                exec("import heapq", namespace)
+                exec("import bisect", namespace)
+                exec("import string", namespace)
+                exec("import sys", namespace)
+                exec("import lctk", namespace)
+                exec("import functools", namespace)
+                exec("import math", namespace)
+                exec("import copy", namespace)
+                exec("import heapq", namespace)
+                exec("import sortedcontainers", namespace)
+
+                exec("from math import floor, ceil, factorial, sqrt, inf", namespace)
+                exec("from sys import maxsize, stdin", namespace)
+                exec("from bisect import bisect_left, bisect_right", namespace)
+                exec("from itertools import permutations, zip_longest", namespace)
+                exec("from heapq import heappush, heappop, heapify", namespace)
+                exec("from collections import deque, defaultdict, OrderedDict", namespace)
+                exec("from typing import List, Optional, Tuple", namespace)
+                exec("from functools import lru_cache, cache", namespace)
+                
+                exec("class ListNode(object):\n\tdef __init__(self, val=0, next=None):\n\t\tself.val = val\n\t\tself.next = next", namespace)
+                exec("class TreeNode(object):\n\tdef __init__(self, val=0, left=None, right=None):\n\t\tself.val = val\n\t\tself.left = left\n\t\tself.right = right", namespace)
+                
+                exec("def print(*args):pass", namespace)
+                
+                total, passed = 0, 0
+                runtime = 0
+                with Sandbox.swallow_io():
+                    with Sandbox.time_limit(sample['timeout']):                        
+                        try:
+                            exec(sample['solution'], namespace)
+                            exec(f"solution=Solution()", namespace)
+                            exec(sample['convert_offline'], namespace)
+                            exec(sample['evaluate_offline'], namespace)
+                        except Exception as e:
+                            result.append({"status": "failed@load", "runtime": runtime, "error": e})
+                            
+                        try:
+                            start_time = time.time()
+                            for test_case in sample['test_cases']:
+                                namespace['inputs'] = test_case['input']
+                                namespace['expected'] = test_case['expected']
+                                exec("inputs, expected = convert_offline((inputs, expected))", namespace)
+                                exec(f"outputs = solution.{sample['entry_point']}(*inputs)", namespace)
+                                exec(f"passed = evaluate_offline(inputs, outputs, expected)", namespace)
+                                total += 1
+                                passed += (1 if namespace['passed'] else 0)
+                            end_time = time.time()
+                            runtime = end_time-start_time
+                        except Exception as e:
+                            result.append({"status": "failed@eval", "runtime": runtime, "error": e})
+                
+                if total == passed:
+                    result.append({"status": "passed", "runtime": runtime, "error": "None"})
+                else:
+                    result.append({"status": "failed@cases", "runtime": runtime, "error": "case error"})
+            except TimeoutException:
+                result.append({"status": "failed@timeout", "runtime": runtime, "error": "execution time out"})
+            except BaseException as e:
+                result.append({"status": "failed@error", "runtime": runtime, "error": e})
+
+            # Needed for cleaning up.
+            shutil.rmtree = rmtree
+            os.rmdir = rmdir
+            os.chdir = chdir
+    
+    @staticmethod
+    def run_sample(sample) -> Dict:
+        """
+        Evaluates the functional correctness of a completion by running the test suite provided in the problem. 
+        """
+
+        with Manager() as manager:
+            result = manager.list()
+
+            p = Process(target=Sandbox.unsafe_execute, args=(sample, result))
+            p.start()
+            p.join(timeout=sample['timeout']+1)
+            if p.is_alive():
+                p.kill()
+
+            if not result:
+                result.append({"status": "failed@timeout", "runtime": sample['timeout'], "error": "sandbox time out"})
+            
+            return dict(
+                result=result[0]['status'], 
+                runtime=result[0]['runtime'], 
+                index=sample['solution_index'], 
+                error=result[0]['error'],
+            )
+
+    @staticmethod
+    def run_samples(samples, n_workers=4):
+        with ThreadPoolExecutor(max_workers=n_workers) as executor:
+            futures, results = list(), list()
+                
+            for sample in samples:
+                args = (sample,)
+                future = executor.submit(Sandbox.run_sample, *args)
+                futures.append(future)
+                n_samples += 1
+            
+            for future in tqdm(as_completed(futures), total=len(futures), desc='Reading futures'):
+                result = future.result()
+                results.append(result)
+        
+        return results
+
+
+def estimate_at_k(num_samples, num_correct, k):
+    """Estimates beyond@k of each problem and returns them in an array."""
+    
+    def cf(n, k):
+        return math.gamma(n+1) / (math.gamma(k+1) * (math.gamma(n-k+1)))
+
+    def estimator(n: int, c: int, k: int) -> float:
+        if n - c < k:
+            return 1.0
+        return 1 - cf(n-c, k) / cf(n, k)
+
+    if isinstance(num_samples, int):
+        num_samples_it = itertools.repeat(num_samples, len(num_correct))
+    else:
+        assert len(num_samples) == len(num_correct)
+        num_samples_it = iter(num_samples)
+
+    return np.array([estimator(int(n), int(c), k) for n, c in zip(num_samples_it, num_correct)])
+
+def compute_beyond_eval(generations_list, reference_list, timeout=30):
+    sandbox = Sandbox()
+    
+    scores = {
+        "Easy": dict(total_c=list(), correct_c=list(), beyond_c=list()),
+        "Medium": dict(total_c=list(), correct_c=list(), beyond_c=list()),
+        "Hard": dict(total_c=list(), correct_c=list(), beyond_c=list()),
+        "Average": dict(total_c=list(), correct_c=list(), beyond_c=list()),
+    }
+    
+    for generations, instance in tqdm(zip(generations_list, reference_list), total=len(generations_list)):
+        # Construct runtime distribution from sample solutions
+        runtimes = list()
+        for index, solution in enumerate(instance['solutions']):
+            sample = {
+				"solution": solution['solution'],
+				"convert_offline": instance['convert_offline'],
+				"evaluate_offline": instance['evaluate_offline'],
+				"entry_point": instance['entry_point'],
+				"test_cases": json.loads(instance['test_cases']),
+    			"solution_index": index,
+				"timeout": timeout
+			}
+            result = sandbox.run_sample(sample)
+            
+            if result['result'] == "passed":
+                runtimes += [result['runtime']]
+        
+        # Calculate Range
+        runtimes = sorted(runtimes)
+        min_runtime = min(runtimes)
+        max_runtime = max(runtimes)
+        
+        # Evaluate generated solutions
+        t_c, p_c, b_c = 0, 0, 0
+        difficulty = instance['difficulty']
+        
+        for index, solution in enumerate(generations):                   
+            sample = {
+                "solution": instance['prompt'] + solution,
+                "convert_offline": instance['convert_offline'],
+                "evaluate_offline": instance['evaluate_offline'],
+                "entry_point": instance['entry_point'],
+                "test_cases": json.loads(instance['test_cases']),
+                "solution_index": index,
+                "timeout": timeout,
+            }
+
+            result = sandbox.run_sample(sample)
+            t_c += 1
+            
+            # Calculate Beyond
+            if result['result'] == "passed":
+                runtime = result['runtime']
+                runtime = min(runtime, max_runtime)
+                runtime = max(runtime, min_runtime)
+                b_c += (max_runtime - runtime) / (max_runtime - min_runtime)          
+                p_c += 1
+            else:
+                runtime = float('inf')
+        
+        scores[difficulty]['total_c'] += [t_c]
+        scores[difficulty]['correct_c'] += [p_c]
+        scores[difficulty]['beyond_c'] += [b_c]
+        
+        scores['Average']['total_c'] += [t_c]
+        scores['Average']['correct_c'] += [p_c]
+        scores['Average']['beyond_c'] += [b_c]
+    
+    results = dict()
+    for difficulty in ['Easy', "Medium", "Hard", "Average"]:
+        total = np.array(scores[difficulty]['total_c'])
+        correct = np.array(scores[difficulty]['correct_c'])
+        beyond = np.array(scores[difficulty]['beyond_c'])
+        
+        pass_at_k = {f"{difficulty}_pass@{k}": estimate_at_k(total, correct, k).mean() for k in [1,3,5] if (total >= k).all()}
+        beyond_at_k = {f"{difficulty}_beyond@{k}": estimate_at_k(total, beyond, k).mean() for k in [1,3,5] if (total >= k).all()}
+        
+        results.update(pass_at_k)
+        results.update(beyond_at_k)
+
+    return results
+
+
+
---
+++ bigcode_eval/tasks/mercury.py
@@ -0,0 +1,75 @@
+"""Mercury: An Efficiency Benchmark for LLM Code Synthesis
+https://arxiv.org/abs/2402.07844
+
+Mercury is the first benchmark to assess the code efficiency of LLM code generation tasks. 
+It consists of 1,889 programming tasks covering diverse difficulty levels alongside test case generators generating unlimited cases for comprehensive evaluation. 
+
+Homepage: https://github.com/Elfsong/Mercury
+"""
+
+
+from bigcode_eval.base import Task
+from bigcode_eval.tasks.custom_metrics.beyond_eval import compute_beyond_eval
+
+_CITATION = """
+@article{du2024mercury,
+  title={Mercury: An Efficiency Benchmark for LLM Code Synthesis},
+  author={Du, Mingzhe and Luu, Anh Tuan and Ji, Bin and Ng, See-Kiong},
+  journal={arXiv preprint arXiv:2402.07844},
+  year={2024}
+}
+"""
+
+
+class Mercury(Task):
+    """
+    A task represents an entire benchmark including its dataset, problems,
+    answers, generation settings and evaluation methods.
+    """
+
+    DATASET_PATH = "Elfsong/Mercury"
+
+    def __init__(self, prompt):
+        super().__init__(
+            stop_words=["\nclass", "\ndef", "\n#", "\n@", "\nprint", "\nif", "\n```", "<file_sep>"],
+            requires_execution=True,
+        )
+        self.prompt = prompt
+
+    def get_dataset(self):
+        """Returns dataset for the task or an iterable of any object, that get_prompt can handle"""
+        return self.dataset["eval"]
+
+    def get_prompt(self, doc):
+        """Builds the prompt for the LM to generate from."""
+        return f'\'\'\'{doc["pretty_content"][0]}\'\'\'\n{doc["prompt"]}'
+    
+    def get_reference(self, doc):
+        """Builds the reference solutions for the doc (sample from the test dataset)."""
+        return doc
+
+    def postprocess_generation(self, generation, idx):
+        """Defines the postprocessing for a LM generation.
+        :param generation: str
+            code generation from LM
+        :param idx: int
+            index of doc in the dataset to which the generation belongs
+            (not used for Humaneval-Task)
+        """
+        prompt = self.get_prompt(self.get_dataset()[idx])
+        generation = generation[len(prompt):]
+        print(generation)
+        return self._stop_at_stop_token(generation, self.stop_words)
+
+    def process_results(self, generations, references):
+        """Takes the list of LM generations and evaluates them against ground truth references,
+        returning the metric for the generations.
+        :param generations: list(list(str))
+            list of lists containing generations
+        :param references: list(str)
+            list of str containing refrences
+        """
+        
+        results = compute_beyond_eval(generations, references)
+        
+        return results
