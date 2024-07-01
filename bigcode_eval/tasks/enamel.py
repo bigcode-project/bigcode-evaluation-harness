d23b9385cacd128d00da2e97eead7d7c2ce8bddf
q-rz <100142775+q-rz@users.noreply.github.com>
2024-07-01 00:13:08 +0000

    Add a new benchmark ENAMEL

--- bigcode_eval/tasks/enamel.py
+++ bigcode_eval/tasks/enamel.py
@@ -26,11 +26,31 @@ class ENAMEL(GeneralHumanEval):
     """
 
     DATASET_PATH = "q-rz/enamel"
-    DATASET_NAME = None
+    DATASET_NAME = "ENAMEL_HumanEval"
+    DATASET_SUBSETS = {
+        "enamel": sorted(set(range(164)) - {2, 23, 41, 45, 53, 60, 71, 92, 97, 99, 102, 123, 124, 135, 137, 138, 144, 148, 156, 157, 159, 160}),
+        "enamel-algo": [10, 18, 36, 39, 40, 43, 46, 49, 55, 59, 63, 76, 83, 96, 107, 109, 114, 129, 147, 154],
+        "enamel-impl": [1, 5, 8, 9, 11, 12, 15, 16, 17, 19, 21, 22, 24, 25, 26, 27, 31, 33, 37, 38, 44, 48, 49, 50, 51, 52, 56, 57, 58, 59, 61, 64, 66, 69, 70, 72, 73, 74, 75, 78, 80, 82, 85, 87, 89, 91, 93, 94, 95, 96, 98, 100, 104, 105, 108, 110, 111, 112, 113, 116, 117, 118, 121, 122, 125, 127, 128, 131, 140, 142, 143, 150, 152, 155, 161],
+        "humaneval": list(range(164)),
+    }
 
-    def __init__(self, strip_prompt, k=[1, 10, 100], num_workers=16, timeout_factor=): # TODO
-        super().__init__(strip_prompt=strip_prompt, k=k, num_workers=num_workers, timeout=None)
-        # TODO
+    def __init__(self,
+        strip_prompt, k=[1, 10, 100], num_workers=16, timeout=20.,
+        subset="enamel", # list of problem IDs, or one of {"enamel", "enamel-algo", "enamel-impl", "humaneval"}
+        hardness=[0., 3., 3., 4.], memory_giga=4., timeout_factor=2., tolerence_sec=0.01, tests_path="cache/eval~tests.pkl",
+    ):
+        super().__init__(strip_prompt=strip_prompt, k=k, num_workers=num_workers, timeout=timeout)
+        if isinstance(subset, list):
+            self.subset = subset
+        else:
+            assert subset in self.DATASET_SUBSETS, f"unknown subset {repr(subset)}"
+            self.subset = self.DATASET_SUBSETS[subset]
+        self.hardness = hardness
+        self.memory_giga = memory_giga
+        self.timeout_factor = timeout_factor
+        self.tolerence_sec = tolerence_sec
+        self.tests_path = tests_path
+        # TODO: load dataset and tests
 
     def get_dataset(self):
         # TODO: retrieve the evaluation subset from the loaded dataset (e.g. `self.dataset["test"]`)
