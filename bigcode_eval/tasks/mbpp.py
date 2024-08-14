960f3f6de2427d14d707169dd6c66422d5f1b30c
Abhineetsoccer <65859971+Abhineetsoccer@users.noreply.github.com>
2024-08-14 20:17:51 +0100

    Update mbpp.py

--- bigcode_eval/tasks/mbpp.py
+++ bigcode_eval/tasks/mbpp.py
@@ -12,9 +12,6 @@ Homepage:: https://github.com/google-research/google-research/tree/master/mbpp
 
 from bigcode_eval.base import Task
 from bigcode_eval.tasks.custom_metrics.code_eval import compute_code_eval
-from transformers import AutoModelForCausalLM, AutoTokenizer
-import torch
-
 
 _CITATION = """
 @article{austin2021program,
@@ -31,40 +28,23 @@ class MBPP(Task):
     answers, generation settings and evaluation methods.
     """
 
-    DATASET_PATH = "/mnt/roma/abhineet/mbpp_withpseudods"
+    DATASET_PATH = "mbpp"
 
     def __init__(self):
         super().__init__(
             stop_words=["\nclass", "\nassert", '\n"""', "\nprint", "\nif", "\n<|/", "\n```"],
             requires_execution=True,
         )
-        checkpoint = "/mnt/roma/abhineet/output_dir_p/checkpoint-5640"
-        self.model = AutoModelForCausalLM.from_pretrained(checkpoint)
-        self.tokenizer = AutoTokenizer.from_pretrained(checkpoint)
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
     def get_dataset(self):
         """Returns dataset for the task or an iterable of any object, that get_prompt can handle"""
-        dataset = self.dataset
+        dataset = self.dataset["test"]
         # the wrong split of mbpp can be loaded with old datasets cache
         assert (
             len(dataset) == 500
         ), "please ensure you have the latest version of MBPP dataset, try deleting its old cache"
         return dataset
 
-    def generate_prompt(self, doc):
-        description = doc["text"]
-        test_example = doc["test_list"][0]
-        prompt = f'"""Task Description: \n\n{description}\n{test_example}\n\nPseudocode:\n\n"""'
-        self.model.to(self.device)
-        inputs = self.tokenizer.encode(prompt, return_tensors = "pt").to(self.device)
-        outputs = self.model.generate(inputs, max_new_tokens = 300)
-        output = self.tokenizer.decode(outputs[0])
-        start_idx = output.find("Code:")
-        return output[:start_idx]
-        
-    
-    
     def get_prompt(self, doc):
         """Builds the prompt for the LM to generate from.
         MBPP prompt is built following to InCoder (Fried et al.) approach
@@ -72,13 +52,7 @@ class MBPP(Task):
         """
         description = doc["text"]
         test_example = doc["test_list"][0]
-        pseudocodes = doc["Pseudocodes"]
-        prompt = f'"""Task Description: \n\n{description}\n{test_example}{pseudocodes}"""\nCode:\n\n'
-        
-        #prompt_with_pseudocode = self.generate_prompt(doc)
-        #prompt = prompt_with_pseudocode + "\n\n\nCode:\n\n"
-        
-        
+        prompt = f'"""\n{description}\n{test_example}\n"""\n'
         return prompt
 
     def get_reference(self, doc):
@@ -93,10 +67,8 @@ class MBPP(Task):
         :param idx: int
             index of doc in the dataset to which the generation belongs
         """
-        prompt = self.get_prompt(self.dataset[idx])
-        start_idx = generation.find("Code:")
-        generation = generation[start_idx + len("Code:"):]
-        print(generation)
+        prompt = self.get_prompt(self.dataset["test"][idx])
+        generation = generation[len(prompt) :]
         return prompt + self._stop_at_stop_token(generation, self.stop_words)
 
     def process_results(self, generations, references):
